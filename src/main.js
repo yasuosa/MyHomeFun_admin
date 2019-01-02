00b5cb5e537cc7b30562109d71a8ee58db798fec
smallwei <1634566606@qq.com>
2019-01-02 12:37:15 +0800

    init avuex cli

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import App from './App';
 import router from './router/router';
 import './permission'; // 权限
 import './error'; // 日志
-// import 'avue-plugin-transfer/packages' //引入avue-plugin-transfer插件
-// import 'avue-plugin-ueditor/packages' //引入avue-plugin-ueditor插件(如果要兼容ie自行换掉富文本编辑器，此款插件不兼容ie)
 import store from './store';
 import { loadStyle } from './util/util'
 import * as urls from '@/config/env';
@@ -16,14 +14,6 @@ import {
 } from '@/config/env';
 import * as filters from './filters' // 全局filter
 import './styles/common.scss';
-// 引入avue的包
-import '@smallwei/avue/lib/index.js';
-// 引入avue的样式文件
-import '@smallwei/avue/lib/theme-chalk/index.css';
-
-// //源文件包
-// import './packages/index.js';
-// import './packages/theme-chalk/src/index.scss';
 
 import basicContainer from './components/basic-container/main'
 
@@ -36,16 +26,16 @@ Vue.use(VueAxios, axios)
 
 //注册全局容器
 Vue.component('basicContainer', basicContainer)
-    // 加载相关url地址
+// 加载相关url地址
 Object.keys(urls).forEach(key => {
     Vue.prototype[key] = urls[key];
 })
 
 // 加载过滤器
 Object.keys(filters).forEach(key => {
-        Vue.filter(key, filters[key])
-    })
-    // 动态加载阿里云字体库
+    Vue.filter(key, filters[key])
+})
+// 动态加载阿里云字体库
 iconfontVersion.forEach(ele => {
     loadStyle(iconfontUrl.replace('$key', ele));
 })
